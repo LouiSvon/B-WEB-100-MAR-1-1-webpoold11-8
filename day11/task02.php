ad826f408ecba0aba5c6b10ec92588ed91a6bb2c
Louis <louisavon77@gmail.com>
2025-10-27 13:28:07 +0100

    day11

--- day11/task02.php
+++ day11/task02.php
@@ -1,5 +1,10 @@
 <?php
-function render_body(home, php, sql){
-
+function render_body($page){
+    if ($page == "home" || $page == "php" || $page == "sql"){
+         $filename = $page . ".html";
+    return file_get_contents($filename);
+    } else {
+        return <p>Unknown page</p>
+    }
 }
 ?>
